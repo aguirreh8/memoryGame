32941bdc3c65b43b253f57580d6de8edcb47471e
unknown <aguirreh8@gmail.com>
2018-04-11 21:31:10 -0400

    added high score tracking

--- src/App.js
+++ src/App.js
@@ -10,23 +10,38 @@ class App extends Component {
   state = {
     characters,
     lastId: 0,
-    counter: 0
+    counter: 0,
+    highScore: 0,
   };
 
   handleIncrement = (id) => {
-    this.setState(
-      { 
-        counter: this.state.counter + 1,
-        lastId: id
-      }
-    )
+    if (this.state.lastId !== id) {
+      this.setState(
+        { 
+          counter: this.state.counter + 1,
+          lastId: id,
+          highScore: 
+            this.state.counter >= this.state.highScore 
+              ? 
+              this.state.counter + 1 : this.state.highScore
+        }
+      )
+    }
+    else {
+      this.setState(
+        {
+          counter: 0,
+        }
+      )
+    }
+
   };
 
   render() {
     return (
       <Wrapper>
         <Title>Memory Game!</Title>
-        <h1>{this.state.counter}</h1>
+        <h1>{this.state.counter} {this.state.highScore}</h1>
         <Main>
           {this.state.characters.map(character => (
             <Characters
